ae2c7808cf865301877c64d199de43a15817fdac
Pedro Takahashi <pedroh.takahashi@gmail.com>
2021-09-13 17:41:34 -0300

    feat(styles):Styles and navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Todo from "./components/Inputs/Todo";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Menutopside } from "./components/Navbar/Menutopside";
+import './assets/styles/app.css'
 
 const App = () => {
 return(
   <div className="app-main">
+    <Menutopside />
     <Todo />
   </div>
 );
